38ff20bb517d92bc3664efe7c50c5c27121d2034
Cedric GESTES <cedric.gestes@gmail.com>
2017-11-08 23:59:51 +0100

    feat(dir): recurse among files
    respect .gitignore, ignore hidden files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+extern crate clap;
+extern crate ignore;
+
+use clap::{App, Arg};
+use ignore::Walk;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("fr")
+        .version("0.1.0")
+        .author("The ruplacer team")
+        .about("Find and replace")
+        .arg(Arg::with_name("PATERN")
+            .help("the pattern to grep for")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("DIR")
+            .short("d")
+            .long("dir")
+            .value_name("DIR")
+            .index(2)
+            .help("the directory to find into")
+            .takes_value(true))
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity"))
+        .get_matches();
+    //println!("{}", matches);
+    let dir = matches.value_of("DIR").unwrap_or("./");
+    println!("Root directory: {}", dir);
+
+    for result in Walk::new(dir) {
+        // Each item yielded by the iterator is either a directory entry or an
+        // error, so either print the path or the error.
+        match result {
+            Ok(entry) => println!("{}", entry.path().display()),
+            Err(err) => println!("ERROR: {}", err),
+        }
+    }
 }
