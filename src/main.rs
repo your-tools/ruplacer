efaabe69734afa2ca106e927164a3c377be66604
Cedric GESTES <cedric.gestes@gmail.com>
2017-11-25 01:39:38 +0100

    feat: add --go

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,61 @@
-extern crate clap;
 extern crate ignore;
 extern crate regex;
 extern crate difference;
 extern crate term;
 extern crate structopt;
-
 #[macro_use]
 extern crate structopt_derive;
-
 
 mod replacer;
 
 use structopt::StructOpt;
 use ignore::Walk;
 use std::path::Path;
-use std::fs::File;
-use std::io::prelude::*;
-use replacer::Replacer;
-
-
-fn replace_file(replacer: &Replacer, file_name: &Path) {
-    if !file_name.is_file() {
-        return
-    }
-    println!("{}", file_name.display());
-
-    let mut f = File::open(file_name).expect("file not found");
-    let mut data = String::new();
-    f.read_to_string(&mut data).expect("error reading file");
-    replacer.grep_or_replace(data.as_str());
-}
+use replacer::{Replacer, FileReplacer};
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "fr",
             version = "0.1.0",
             author = "The ruplacer team",
             about = "Find and Ruplace patterns in files and filenames.")]
-struct Opt {
+struct ReplacerOpts {
     #[structopt(short = "v", long = "verbose", help = "Activate verbose mode")]
     debug: bool,
 
-    #[structopt(help = "The pattern to grep for")]
+    #[structopt(long = "go", help = "Perform changes rather than just printing them")]
+    go: bool,
+
+    #[structopt(help = "The pattern to search for")]
     pattern: String,
 
     #[structopt(help = "The replacement")]
     replacement: Option<String>,
 
-    #[structopt(help = "paths (default to current working directory)")]
+    #[structopt(help = "paths (default to current working directory)", default_value="./")]
     paths: Vec<String>,
 }
 
-fn main() {
-    let opts = Opt::from_args();
-    let dirs: Vec<_> = match opts.paths.len() {
-        0 => vec!["./".to_string()],
-        _ => opts.paths,
-    };
+fn walk_file(replacer: &Replacer, opts: &ReplacerOpts, file_name: &Path) {
+    replacer.replace_in_file(file_name, !opts.go);
+}
 
-    let replacer = Replacer::new(opts.pattern.as_str(), opts.replacement);
-
-    for dir in dirs {
+fn walk_paths(replacer: &Replacer, opts: &ReplacerOpts) {
+    for dir in opts.paths.iter() {
         println!("Root directory: {}", dir);
         for result in Walk::new(dir) {
             // Each item yielded by the iterator is either a directory entry or an
             // error, so either print the path or the error.
             match result {
-                Ok(entry) => replace_file(&replacer, entry.path()),
+                Ok(entry) => walk_file(&replacer, opts, entry.path()),
                 Err(err) => println!("ERROR: {}", err),
             }
         }
     }
+}
+
+fn main() {
+    let opts = &ReplacerOpts::from_args();
+
+    let replacer = Replacer::new(opts.pattern.as_str(), opts.replacement.clone());
+    walk_paths(&replacer, &opts)
 }
--- src/replacer/mod.rs
+++ src/replacer/mod.rs
@@ -1,6 +1,10 @@
 use term;
 use regex::Regex;
 use difference::{Changeset, Difference};
+use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{Result as IoResult, Error as IoError, ErrorKind};
 
 pub struct Replacer {
     re: Regex,
@@ -34,6 +38,32 @@ fn term_show_diff(before: &str, after: &str) {
     t.flush().unwrap();
 }
 
+pub trait FileReplacer {
+    fn replace_in_file(&self, file_name: &Path, dry_run: bool) -> IoResult<()>;
+}
+
+impl FileReplacer for Replacer {
+    fn replace_in_file(&self, file_path: &Path, dry_run: bool) -> IoResult<()> {
+        if !file_path.is_file() {
+            return Err(IoError::from(ErrorKind::InvalidInput))
+        }
+        println!("{}", file_path.display());
+
+        let mut data = String::new();
+        {
+            let mut f = File::open(file_path).expect("file not found");
+            f.read_to_string(&mut data).expect("error reading file");
+        }
+        let new_data = self.replace(data.as_str());
+        if !dry_run {
+            println!("TODO: implement replace");
+            // Recreate the file and dump the processed contents to it
+            let mut dst = File::create(&file_path).expect("error opening the file for writing");
+            dst.write(new_data.as_bytes()).expect("error writing to file");
+        }
+        Ok(())
+    }
+}
 
 impl Replacer {
     pub fn new(pattern: &str, replacement: Option<String>) -> Replacer {
@@ -47,8 +77,8 @@ impl Replacer {
     pub fn grep_or_replace(&self, buf: &str) {
         match self.replacement {
             None => self.grep(buf),
-            Some(_) => self.replace(buf),
-        }
+            Some(_) => {self.replace(buf);},
+        };
     }
 
     pub fn grep(&self, _buf: &str) {
@@ -58,13 +88,14 @@ impl Replacer {
         // }
     }
 
-    pub fn replace(&self, buf: &str) {
+    pub fn replace(&self, buf: &str) -> String {
         let replacement = self.replacement.as_ref().unwrap();
         let after = self.re.replace_all(buf, replacement.as_str());
 
         if buf != after {
             term_show_diff(buf, &after)
         }
+        after.to_string()
     }
 }
 
