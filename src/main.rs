3112f2e3bec3ffc33724734b320c8b1474714129
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-10-09 14:55:38 +0200

    Add a working main()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[macro_use]
+extern crate structopt;
+use std::path::{Path, PathBuf};
+use std::process;
+use structopt::StructOpt;
+
+extern crate ruplacer;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "ruplacer")]
+struct Opt {
+    #[structopt(long = "go")]
+    go: bool,
+
+    #[structopt(help = "The pattern to search for")]
+    pattern: String,
+
+    #[structopt(help = "The replacement")]
+    replacement: String,
+
+    #[structopt(parse(from_os_str), help = "The source path. Defaults to the working directory")]
+    path: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    let dry_run = !opt.go;
+
+    let path = opt.path.unwrap_or(Path::new(".").to_path_buf());
+    let mut replacer = ruplacer::Replacer::new(path);
+    replacer.dry_run(dry_run);
+    let outcome = replacer.replace(&opt.pattern, &opt.replacement);
+    if let Err(err) = outcome {
+        eprintln!("{}", err);
+        process::exit(1);
+    }
 }
